c16d5df9c2d92ae6c5abdaa77098c22082d97039
DiegoLuQ <jdluquelinares@gmail.com>
2022-03-30 00:57:21 -0300

    leyendo json presupuesto y agregando DB

--- db.py
+++ db.py
@@ -3,5 +3,5 @@ import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
 load_dotenv()
-#conn = MongoClient(os.getenv('DATABASE'))
-conn = MongoClient()
+conn = MongoClient(os.getenv('DATABASE'))
+#conn = MongoClient()
